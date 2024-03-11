88c8f552c242ea369a94d5497f82f7cd3c4fd6c8
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-03-11 18:34:47 -0300

    feat: cri primeira versão do componente about

--- cloudFire/src/views/HomeView.vue
+++ cloudFire/src/views/HomeView.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import HomeHeader from '../components/HomeHeader.vue'
 import MainHome from '../components/MainHome.vue'
+import AboutHome from '../components/AboutHome.vue'
 </script>
 
 <template>
   <main>
     <HomeHeader />
     <MainHome />
+    <AboutHome />
   </main>
 </template>
