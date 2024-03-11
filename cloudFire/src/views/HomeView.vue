603139bb7597bd3edb0fb9be1e84690b88189186
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-03-11 17:52:32 -0300

    feat: cria componete main da página home
    possui um h1, um parágrafo, um botao e uma imagem

--- cloudFire/src/views/HomeView.vue
+++ cloudFire/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
 import HomeHeader from '../components/HomeHeader.vue'
+import MainHome from '../components/MainHome.vue'
 </script>
 
 <template>
   <main>
     <HomeHeader />
+    <MainHome />
   </main>
 </template>
